2bc75e792b7ef2d9c1e6cc0fc4bcba26b2ce1313
Fj-martinez33 <fcojmm91@gmail.com>
2024-10-03 20:36:07 +0000

    finished

--- exercises/10-Remove-vowels/app.js
+++ exercises/10-Remove-vowels/app.js
@@ -1,5 +1,13 @@
 // Your code goes here
-
+const rapid = (name) => {
+    let consonants = []
+    for (let i of name.toLowerCase()){
+        if(['a', 'e', 'i', 'o', 'u'].includes(i) == false){
+            consonants.push(i.toUpperCase());
+        }
+    }
+    return consonants;
+}
 
 // Work above this line; do not change code below
 let str = "John";
